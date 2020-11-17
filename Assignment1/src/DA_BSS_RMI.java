8631ab1affe5c22bae6a6b11b08bbf622ade4a89
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2020-11-17 20:28:50 +0100

    Update parameters

--- Assignment1/src/DA_BSS_RMI.java
+++ Assignment1/src/DA_BSS_RMI.java
@@ -10,15 +10,15 @@ public interface DA_BSS_RMI extends Remote {
      * Method that broadcasts a message to other methods
      * Content of the message can be of any type, we assume text for now.
      */
-    void broadcast(Message m) throws RemoteException;
+    void broadcast(String text) throws RemoteException;
 
     /**
      * Method that receives a message, consisting of content, logical clock and receiver information.
      */
-    void receive(Message m) throws RemoteException;
+    void receive(Message msg) throws RemoteException;
 
     /**
      * Method that delivers a message if it is able to deliver it.
      */
-    void deliver(Message m) throws RemoteException;
+    void deliver(Message msg) throws RemoteException;
 }
