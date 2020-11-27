d0bb4590b8401cbd1b74fd9fb9aad018ebddfdc7
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2020-11-27 15:18:23 +0100

    Add seperate classes

--- Assignment2/src/DA_PSON_Main.java
+++ Assignment2/src/DA_PSON_Main.java
@@ -1,68 +1,72 @@
-import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.util.ArrayList;
 
 public class DA_PSON_Main {
-    public static void main(String[] args){
-        ArrayList<String> ipList = new ArrayList<>();
-        ArrayList<DA_PSON_Component> ComponentList= new ArrayList<DA_PSON_Component>();
 
-        for(int i=0;i<10;i++){
-            String currentIP = "rmi://localhost:110"+i;
-            ipList.add(currentIP);
+    private static ArrayList<DA_PSON_Component> componentList = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        //Creating the unidirectional ring as presented in the slides
+        //          7
+        //      5       4
+        //  6               9
+        //  2               12
+        //      8       1
+        //          3
+        //Each machine will create half of the ring
+        int[] ownIds = {3, 8, 2, 6, 5};
+        int[] otherIds = {7, 4, 9, 12, 1};
+
+        int defaultport = 1099;
+        //my ip address
+        String ownIP = "192.168.0.171";
+        //the ip address of the other host
+        String otherIP = "192.168.0.174";
+        try {
+            LocateRegistry.createRegistry(defaultport);
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
-        int[] exampleFromSlides = {7,4,9,12,1,3,8,2,6,5};
-        for(int i=0;i<ipList.size();i++){
-            try {
-                java.rmi.registry.LocateRegistry.createRegistry(1100+i);
-                DA_PSON_Component currentComponent = new DA_PSON_Component(exampleFromSlides[i], "EMPTY");
-                Naming.rebind(ipList.get(i) +"//DA_PSON_Component", currentComponent);
-                ComponentList.add(currentComponent);
-            } catch (RemoteException | MalformedURLException e) {
-                e.printStackTrace();
+        //create x process, where x is ids length
+        for (int i = 0; i < ownIds.length; i++) {
+            //lookup is of the shape: RMI+IP+PORT+ID
+            String ownLookup = "rmi://" + ownIP + ":" + defaultport + "/" + Integer.toString(ownIds[i]);
+            String nextLookup;
+            //For the last one in this half of the circle, we have to set the nextLookup to the first id of the otherIDs
+            if (i != ownIds.length - 1) {
+                nextLookup = "rmi://" + ownIP + ":" + defaultport + "/" + Integer.toString(ownIds[i + 1]);
+            } else {
+                nextLookup = "rmi://" + otherIP + ":1099/" + Integer.toString(otherIds[0]);
             }
-        }
-        for(int i=0;i<ipList.size();i++){
-            //setNext for each node
-            int nextint = i+1;
-            if(nextint==ipList.size()){
-                nextint=0;
-            }
-            ComponentList.get(i).setNextString(ipList.get(nextint)+"//DA_PSON_Component");
+            //Call the setup component function, which assigns the Strings of the next and own component
+            setupComponent(ownIds[i], ownLookup, nextLookup);
         }
 
-        ComponentList.get(0).performElectionRound();
-        ComponentList.get(0).performElectionRound();
 
+        int numrounds = 1;
 
-        //hardcode the example from the slides
-//        try {
-//            ComponentList.add(new DA_PSON_Component(7));
-//            ComponentList.add(new DA_PSON_Component(4));
-//            ComponentList.add(new DA_PSON_Component(9));
-//            ComponentList.add(new DA_PSON_Component(12));
-//            ComponentList.add(new DA_PSON_Component(1));
-//            ComponentList.add(new DA_PSON_Component(3));
-//            ComponentList.add(new DA_PSON_Component(8));
-//            ComponentList.add(new DA_PSON_Component(2));
-//            ComponentList.add(new DA_PSON_Component(6));
-//            ComponentList.add(new DA_PSON_Component(5));
-//        } catch (RemoteException e) {
-//            e.printStackTrace();
-//        }
-//        for(int i=0;i<ComponentList.size();i++){
-//            int nextIndex = i+1;
-//            if(nextIndex==ComponentList.size()){
-//                nextIndex=0;
-//            }
-//            ComponentList.get(i).setNext(ComponentList.get(nextIndex));
-//        }
+        // Do the rounds forever
+        while(true) {
+            System.out.println("Starting round " + numrounds);
+            componentList.get(0).performElectionRound();
+            System.out.println();
+            numrounds++;
+            Thread.sleep(1000);
+        }
+    }
 
-//        ComponentList.get(0).performElectionRound();
-//        ComponentList.get(1).performElectionRound();
-
-
-
+    private static void setupComponent(int id, String own, String next) {
+        try {
+            //Create the component, with id equalling your own id and next equalling the lookup-String of the next component in line
+            DA_PSON_Component component = new DA_PSON_Component(id, next);
+            //Bind the component to its string
+            Naming.rebind(own, component);
+            //Add component to componentlist
+            componentList.add(component);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
